4770cba601026ecbd945cf2896c323585e327b8d
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-06-29 00:08:53 +0200

    feat: end lesson about error handling and panic

--- src/bin/lesson_44.rs
+++ src/bin/lesson_44.rs
@@ -1,5 +1,9 @@
 // 44: Error handling in Rust - time to panic!
 
 fn main() {
-    
+    // let x: [i32; 2] = [1, 2];
+    // println!("{}", x[2]);
+    // panic!("Rusty ran away!");
+    let v: Vec<i32> = vec![1, 2, 3];
+    v[99];
 }
