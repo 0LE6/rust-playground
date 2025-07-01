724826d65006a5ec6ac7bee974e93e286c1f8ab8
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-01 22:16:03 +0200

    feat: end lesson 36

--- src/bin/lesson_36.rs
+++ src/bin/lesson_36.rs
@@ -1,10 +1,23 @@
 // 36: Structs provide clarity in Rust
 
 fn main() {
-    let rect = (20, 30); // emulating x & y
-    dbg!(get_area(rect));
+    // let rect = (20, 30); // emulating w & h
+    let rect = Rectangle { width: 20, height: 30 };
+    dbg!(get_area(&rect));
+    print!("The rect w/ print {:?}", rect);
+    dbg!(rect);
 }
 
-fn get_area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
+
+fn get_area(rect: &Rectangle) -> u32 {
+    rect.width * rect.height 
+}
+
+// fn get_area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
