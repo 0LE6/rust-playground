14be737d4eab90b50a75db4a07344577902aeda4
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-06 20:13:29 +0200

    feat: end of lesson 47

--- src/bin/lesson_47.rs
+++ src/bin/lesson_47.rs
@@ -1,5 +1,32 @@
 // 47: The Try operator (?) is amazing in Rust
 
-fn main() {
+// for the first testing
+// use std::fs::{self, File};
+// use std::io::{self, Read};
 
+// fro the last one
+use std::fs;
+use std::io;
+
+// fn get_data() -> Result<String, io::Error> {
+//     // let mut result = String::new();
+//     // File::open("result.txt")?.read_to_string(&mut result)?;
+//     // Ok(result)
+//    
+//     fs::read_to_string("result.txt")
+// }
+
+fn last_char_of_first_line(text: &str) -> Option<char>{
+   text.lines().next()?.chars().last() 
+}
+
+fn main() {
+   let c = last_char_of_first_line("Hey, you're Rusty\n or not..."); // las char of the line would be 'y' 
+    dbg!(c);
+
+    let empty = last_char_of_first_line("");
+    dbg!(empty); // here we'll get a None
+
+    // let result = get_data();
+    // dbg!(result); // we'll obtain an error 'cause there's no file  
 }
