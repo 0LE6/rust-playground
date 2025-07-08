d7f422fe000fdfca9ef161e60823ae3de121a303
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-08 22:12:44 +0200

    feat: add 2 case scenarios to test

--- src/bin/lesson_48.rs
+++ src/bin/lesson_48.rs
@@ -1,5 +1,7 @@
 // 48: When to panic! vs. when to use Result in Rust
 
+/*
+// --- 1st part ---
 fn main() {
     let word = "Rusty";
     let first_char = get_first_char(word);
@@ -7,10 +9,29 @@ fn main() {
     dbg!(first_char);
 }
 
-fn get_first_char(string: &str) -> char{
-   if string.is_empty() {
+fn get_first_char(string: &str) -> char {
+    if string.is_empty() {
         panic!("String is empty, lmao!");
     }
 
     string.chars().next().unwrap()
+}
+*/
+
+/*
+//--- 2nd part ---
+use std::net::IpAddr;
+
+fn main() {
+    let home: IpAddr = "helloooooooooooooo"
+        .parse()
+        .expect("Hardcoded IP is invalid!");
+
+    dbg!(home);
+}
+*/
+
+// --- 3rd part ---
+fn main() {
+
 }
