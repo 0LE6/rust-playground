e79ddc93f7cd1a007ac27baca6cf86ef9c5e2634
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-13 20:23:27 +0200

    feat: start lesson 51 - vectors

---
+++ src/bin/lesson_51.rs
@@ -0,0 +1,12 @@
+// 51: Vectors are awesome in Rust
+
+fn main() {
+    // let v:Vec<i32> = Vec::new();
+    // dbg!(v);
+
+    let v = vec![1, 2, 3];
+    // print!("v={:?}", v);
+    dbg!(v);
+
+
+}
