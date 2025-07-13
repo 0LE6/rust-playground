e33ce69b7a1982d33d22e920331257693aee3b51
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-13 20:47:15 +0200

    feat: update with push & pop

--- src/bin/lesson_51.rs
+++ src/bin/lesson_51.rs
@@ -1,12 +1,27 @@
 // 51: Vectors are awesome in Rust
 
 fn main() {
+    // 1) start example
     // let v:Vec<i32> = Vec::new();
     // dbg!(v);
 
-    let v = vec![1, 2, 3];
+    // 2) basic example
+    // let v = vec![1, 2, 3];
     // print!("v={:?}", v);
-    dbg!(v);
+    // dbg!(v);
 
+    // 3) adition example
+    // let mut numbers = vec![0];
+    //
+    // numbers.push(69);
+    // numbers.push(420);
+    // numbers.push(3);
+    // 
+    // // dbg!(numbers); 
+    // print!("v={:?} \n", numbers); 
+    // numbers.pop();
+    // print!("v={:?} \n", numbers); 
+
+    // 4) referece example
 
 }
