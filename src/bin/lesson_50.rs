0f754bbabcf07dc83c6222b38ca52785b0cb0510
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-15 23:14:01 +0200

    feat: add second part of the lesson 50

--- src/bin/lesson_50.rs
+++ src/bin/lesson_50.rs
@@ -1,13 +1,45 @@
 // 50: This makes programming in Rust much more convenient
 
 fn main() {
+    // 1)
     // order_food("banana);
     // order_food_fast_mode("Peaches");
+    
+    // 2)
+    let path = String::from("secret.txt");
+    let custom_file = CustomFile { path };
+
+    custom_file.read();
+    custom_file.mocosoft_copailot();
+
+    
+}
+
+// 2) More examples cases
+struct CustomFile {
+    path: String
+}
+
+impl CustomFile {
+    fn read(&self) {
+        println!("Reading: '{}'", &self.path);
+    }
+
+    fn copy(&self) {
+        todo!("copy() still has to be coded!")
+    }
+
+    fn delete(&self) {
+        todo!("delete() still has to be coded!")
+    }
+
+    fn mocosoft_copailot(&self) {
+        unimplemented!("Designed for Mocosoft!")
+    }
 }
 
 
-
-// todo VS. unimplemented
+// 1) todo VS. unimplemented
 // fn order_food(food: &str) {
 //     todo!()
 // }
