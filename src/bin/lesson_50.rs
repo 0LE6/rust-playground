8177a2f93c8d83d969b4d8852e6a3b500459b328
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-15 22:59:51 +0200

    feat: add lesson 50, todo! VS. unimplemented!

---
+++ src/bin/lesson_50.rs
@@ -0,0 +1,17 @@
+// 50: This makes programming in Rust much more convenient
+
+fn main() {
+    // order_food("banana);
+    // order_food_fast_mode("Peaches");
+}
+
+
+
+// todo VS. unimplemented
+// fn order_food(food: &str) {
+//     todo!()
+// }
+//
+// fn order_food_fast_mode(food: &str) {
+//     unimplemented!("Potential concept!")
+// }
