10f47518bd411f14a2f2b94d86bf55f3f7a8b14c
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-18 19:20:17 +0200

    feat: lesson 29, wonts and wills of passing rferences

--- src/bin/lesson_29.rs
+++ src/bin/lesson_29.rs
@@ -1,11 +1,32 @@
 // 29: Mutable references in Rust
 
 fn main() {
-    let mut text = String::from("Rusty");
-    modify(&mut text);
-    dbg!(text);
+    // 1)
+    // let mut text = String::from("Rusty");
+    // modify(&mut text);
+    // dbg!(text);
+
+    // 2)
+    let mut text = String::from("Frosty");
+
+    // this won't work
+    // let r1 = &mut text;
+    // let r2 = &mut text; // cannot borrow `text` as mutable more than once at a time
+    // dbg!(r1, r2); 
+    
+    // this will
+    let r1 = &text;
+    let r2 = &text;
+    dbg!(r1, r2);
+
+    let r3 = &mut text;
+    dbg!(r3);
+
+
+
 }
 
-fn modify(text: &mut String) {
-    text.push_str(" is cool!");
-}
+// 1)
+// fn modify(text: &mut String) {
+//     text.push_str(" is cool!");
+// }
