c9e0f9abe58dfd6cadacd5296e2129597c01e1aa
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-20 19:58:09 +0200

    feat: end lesson 28 'bout refs

--- src/bin/lesson_28.rs
+++ src/bin/lesson_28.rs
@@ -1,13 +1,21 @@
 // 28: References in Rust
 
 fn main() {
-    let txt = String::from("Rusty");
-    let len = get_length(&txt);
+    // let txt = String::from("Rusty");
+    // let len = get_length(&txt);
+    //
+    // println!("The lenght of {txt}: {len}");
+    //
+    // // dbg!(txt, len); // we took ownership, use better a ref 
+    // dbg!(&txt, &len); // better way
+   
+    let mut txt = String::from("Rusty");
+    modify(&mut txt);
+    dbg!(&txt);
+}
 
-    println!("The lenght of {txt}: {len}");
-
-    // dbg!(txt, len); // we took ownership, use better a ref 
-    dbg!(&txt, &len);
+fn modify(txt: &mut String) {
+    txt.push('?');
 }
 
 fn get_length(txt: &str) -> usize {
