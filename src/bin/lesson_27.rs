f2350a4ff43aa77ca62046d53bc09e4069aa35e7
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-21 20:51:49 +0200

    feat: add example 2 about Ownership

--- src/bin/lesson_27.rs
+++ src/bin/lesson_27.rs
@@ -1,18 +1,30 @@
 // 27: Ownership in Rust
 
 fn main() {
-    let name = String::from("Rusty");
-    greet(name); // fn takes Ownership and the value ain't valid anymore 
     
-    let n = 69;
-    display_number(n);
-    println!("Second attempt to use number: {n}"); // this will work 'cause i32 uses copy trait
+    // 2)
+    let s1 = create_string();
+    let s2 = create_string();
+    dbg!(s1, s2); // dbg macro took Ownership
+
+    // 1)
+    // let name = String::from("Rusty");
+    // greet(name); // fn takes Ownership and the value ain't valid anymore 
+    // 
+    // let n = 69;
+    // display_number(n);
+    // println!("Second attempt to use number: {n}"); // this will work 'cause i32 uses copy trait
 }
 
-fn greet(name: String) {
-    println!("Hello, {name}!");
+fn create_string() -> String {
+    String::from("Rusty") 
 }
 
-fn display_number(n: i32) {
-    println!("The number is {n}");
-}
+// 1)
+// fn greet(name: String) {
+//     println!("Hello, {name}!");
+// }
+//
+// fn display_number(n: i32) {
+//     println!("The number is {n}");
+// }
