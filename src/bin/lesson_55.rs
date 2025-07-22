cb27fcf141ad2c34d434e0fe5fb807031bff8c59
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-22 22:14:56 +0200

    feat: little example of iteration

--- src/bin/lesson_55.rs
+++ src/bin/lesson_55.rs
@@ -10,11 +10,16 @@ fn main() {
     // dbg!(scores);
     
     // access
-    let score = scores
-        .get("Rusty")
-        .copied() // copy to not get just a &i32 ref (only if it implements Copy trait)
-        .unwrap_or(0); // unwrap it with 0 if it doesn't exist
-    
-    dbg!(score);
+    // let score = scores
+    //     .get("Rusty")
+    //     .copied() // copy to not get just a &i32 ref (only if it implements Copy trait)
+    //     .unwrap_or(0); // unwrap it with 0 if it doesn't exist
+    // 
+    // dbg!(score);
+
+    // iterate
+    for (k, v) in &scores {
+        println!("{k} : {v} points"); 
+    }
 
 }
