21d81a3df952636581ab6c9d3fe4b692258f3761
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-22 22:12:10 +0200

    feat: add example of how get values from HashMap

--- src/bin/lesson_55.rs
+++ src/bin/lesson_55.rs
@@ -3,8 +3,18 @@
 use std::collections::HashMap;
 
 fn main() {
+    // creation
     let mut scores = HashMap::new();
     scores.insert(String::from("Rusty"), 69);
     scores.insert(String::from("Frosty"), 75);
-    dbg!(scores);
+    // dbg!(scores);
+    
+    // access
+    let score = scores
+        .get("Rusty")
+        .copied() // copy to not get just a &i32 ref (only if it implements Copy trait)
+        .unwrap_or(0); // unwrap it with 0 if it doesn't exist
+    
+    dbg!(score);
+
 }
