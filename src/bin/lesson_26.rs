1980d94638bf41b1815109b4602ffd758d9b21b7
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-23 20:59:34 +0200

    feat: implicit Copy trait var types

--- src/bin/lesson_26.rs
+++ src/bin/lesson_26.rs
@@ -8,9 +8,23 @@ fn main() {
     // print!("Hello, {var}");
    
     // 2) - another example
-    let var = String::from("Rusty");
-    let var2 = var; // takes the value and 'var' is no longer exists
-    // dbg!(var); // value moved and doesn't exist 
-    dbg!(var2); // now the value its in the 'var2'
-    
+    // let var = String::from("Rusty");
+    // let var2 = var; // takes the value and 'var' is no longer exists
+    // // dbg!(var); // value moved and doesn't exist 
+    // dbg!(var2); // now the value its in the 'var2'
+   
+    // 3)
+    let name = String::from("Frosty");
+    let name_copy = name.clone();
+
+    dbg!(name, name_copy);
+
+    // but we don't need to do it with Integers
+    let n: i32 = 69;
+    let n2 = n;
+
+    // this works fine 'cause small types as Integres, Booleans... 
+    // implements the Copy trait inside
+    dbg!(n, n2);
+
 }
