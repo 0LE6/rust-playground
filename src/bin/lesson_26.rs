9a877afbfe624c41d858d026cee5ca79053ad927
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-23 20:52:27 +0200

    feat: example 2

--- src/bin/lesson_26.rs
+++ src/bin/lesson_26.rs
@@ -1,9 +1,16 @@
 // 26: cloning in Rust
 
 fn main() {
-    let mut var = String::from("Rusty"); // Rusty is released from memory
-    var = String::from("Frosty"); // and its Frosty takes its place
-
-    print!("Hello, {var}");
-        
+    // 1)
+    // let mut var = String::from("Rusty"); // Rusty is released from memory
+    // var = String::from("Frosty"); // and its Frosty takes its place
+    //
+    // print!("Hello, {var}");
+   
+    // 2) - another example
+    let var = String::from("Rusty");
+    let var2 = var; // takes the value and 'var' is no longer exists
+    // dbg!(var); // value moved and doesn't exist 
+    dbg!(var2); // now the value its in the 'var2'
+    
 }
