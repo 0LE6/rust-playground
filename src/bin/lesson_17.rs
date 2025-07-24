ba1bab88d06aed3bcd85f88a748b5ece93db72b8
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-24 21:29:43 +0200

    feat: end lesson 17

--- src/bin/lesson_17.rs
+++ src/bin/lesson_17.rs
@@ -1,8 +1,13 @@
 // 17: "Let if" in Rust
 
 fn main() {
-    let n = 11;
-    let odd_even = if n % 2 == 0 {"Even"} else {"Odd"};
-
-    dbg!(odd_even);
+    // let n = 11;
+    // let odd_even = if n % 2 == 0 {"Even"} else {"Odd"};
+    //
+    // dbg!(odd_even);
+    
+    let is_connected = false;
+    let result = if is_connected {0} else {-1};
+    
+    dbg!(result);
 }
