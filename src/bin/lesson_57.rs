6d5d3fb60ec7aa1210a6c737d3cd78242471a51b
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-28 21:17:30 +0200

    feat: HashSet doesn't take repeated values

--- src/bin/lesson_57.rs
+++ src/bin/lesson_57.rs
@@ -5,8 +5,8 @@ use std::collections::HashSet;
 fn main() {
     let mut nums: HashSet<i32> = HashSet::new();
     nums.insert(10); 
-    nums.insert(10); 
-    nums.insert(10); 
+    nums.insert(20); 
+    nums.insert(10); // will not insert a repeated value 
     dbg!(nums);
 
 
