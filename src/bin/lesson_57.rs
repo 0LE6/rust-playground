e7b959dd43280c1e5800dc274b6af0fc44cb72be
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-28 21:15:03 +0200

    feat: lesson 57, about HashSet

--- src/bin/lesson_57.rs
+++ src/bin/lesson_57.rs
@@ -1,5 +1,13 @@
-// 57: Hash maps in Rust (3)
+// 57: Hash set in Rust
+
+use std::collections::HashSet;
 
 fn main() {
-    
+    let mut nums: HashSet<i32> = HashSet::new();
+    nums.insert(10); 
+    nums.insert(10); 
+    nums.insert(10); 
+    dbg!(nums);
+
+
 }
