228f5070be1ae2c3768405c34af55b779ba81ccf
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-29 22:44:05 +0200

    feat: example inserting a vec into set

--- src/bin/lesson_57.rs
+++ src/bin/lesson_57.rs
@@ -3,11 +3,18 @@
 use std::collections::HashSet;
 
 fn main() {
-    let mut nums: HashSet<i32> = HashSet::new();
-    nums.insert(10); 
-    nums.insert(20); 
-    nums.insert(10); // will not insert a repeated value 
-    dbg!(nums);
+    // 1)
+    // let mut nums: HashSet<i32> = HashSet::new();
+    // nums.insert(10); 
+    // nums.insert(20); 
+    // nums.insert(10); // will not insert a repeated value 
+    // dbg!(nums);
 
+    // 2)
+    let mut nums = HashSet::from([10, 20, 10]);
+    let vec = vec![1, 2, 3, 4];
+    let mut hashed_vec: HashSet<i32> = [1, 2, 3, 4].into();
+    
+    dbg!(nums, hashed_vec);
 
 }
