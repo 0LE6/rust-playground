581bb55aa9fa01196f259b976dd76ef46fcea723
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-07-29 23:11:04 +0200

    fix: the previous commit example

--- src/bin/lesson_57.rs
+++ src/bin/lesson_57.rs
@@ -12,8 +12,9 @@ fn main() {
 
     // 2)
     let mut nums = HashSet::from([10, 20, 10]);
-    let vec = vec![1, 2, 3, 4];
-    let mut hashed_vec: HashSet<i32> = [1, 2, 3, 4].into();
+    let vec = vec![1, 2, 3, 4, 4, 3, 5];
+    // convert Vec to HasSet
+    let mut hashed_vec: HashSet<i32> = vec.into_iter().collect();
     
     dbg!(nums, hashed_vec);
 
