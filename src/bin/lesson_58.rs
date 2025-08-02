04ff8fd9c02ff336ec3291c6a9db60e60dc5896e
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-02 13:38:11 +0200

    feat: explanation about crates

--- src/bin/lesson_58.rs
+++ src/bin/lesson_58.rs
@@ -1,5 +1,12 @@
 // 58
 
+// Crates
+//
+// rustc src/bin/lesson_58.rs 
+// creates an executable with the name "lesson_58"
+//
+// if we execute that file it'll give us the print of "Lesson 58"
+
 fn main() {
-    
+    println!("Lesson 58"); 
 }
