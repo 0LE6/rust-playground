ba8a061040ddc4013837d84e8ded813995af82fd
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-04 21:30:52 +0200

    feat: almost complete lesson with good examples of use an own module

--- src/bin/lesson_59.rs
+++ src/bin/lesson_59.rs
@@ -4,4 +4,11 @@ mod bank;
 fn main() {
     let mut acc = bank::accounts::Account::new("Rusty");
     println!("[ACCOUNT] Created: {:?}", acc);
+
+    bank::transactions::deposit(&mut acc, 150);
+    bank::transactions::withdraw(&mut acc, 20);
+
+    println!("[ACCOUNT] Final state: {:?}", acc);
+
+    bank::announce("Maintanance at 01:30 PM");
 }
