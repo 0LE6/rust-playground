0bc8d685a68e511eb37490fae761579082cc03a2
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-07 21:24:17 +0200

    feat: add i32 example lol

--- src/bin/lesson_08.rs
+++ src/bin/lesson_08.rs
@@ -1,8 +1,14 @@
 // 08: data types
 
 fn main() {
+    // boolean
     let conn: bool = false;
     let rec: bool = true;
 
     dbg!(conn, rec);
+
+    // integers
+    let money: i32 = 5_000;
+
+    dbg!(money, money > 0);
 }
