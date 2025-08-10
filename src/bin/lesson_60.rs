f3ff65eb14ba1b1426ed3627af0e4be05f96e9a5
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-10 21:00:41 +0200

    feat: first testing

--- src/bin/lesson_60.rs
+++ src/bin/lesson_60.rs
@@ -2,5 +2,8 @@
 use rust_playground::banking::{accounts, transactions};
 
 fn main() {
-    
+    let mut rusty = accounts::open_account(1);
+    let mut frosty = accounts::open_account(2);
+
+    dbg!(&rusty, &frosty);
 }
