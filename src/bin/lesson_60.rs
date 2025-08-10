148feaee21c763557163a3bac394a6f45ad839c0
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-10 21:03:58 +0200

    feat: end lesson 60 about creating own library

--- src/bin/lesson_60.rs
+++ src/bin/lesson_60.rs
@@ -6,4 +6,10 @@ fn main() {
     let mut frosty = accounts::open_account(2);
 
     dbg!(&rusty, &frosty);
+
+    transactions::deposit(&mut rusty, 200.0);
+    transactions::withdraw(&mut rusty, 50.0);
+    transactions::transfer(&mut rusty, &mut frosty, 100.0);
+
+    dbg!(&rusty, &frosty);
 }
