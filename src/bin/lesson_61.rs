aed6a31400a59f2897cec601c9f781edb2f0ad77
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-17 00:01:55 +0200

    feat: add example 0, redundant code

--- src/bin/lesson_61.rs
+++ src/bin/lesson_61.rs
@@ -1,5 +1,19 @@
 // 61: generic functions in Rust
 
+// Example 0: redundant code with duplicated logics
+// (to solve in next example)
 fn main() {
-    
+    let numbers = vec![1, 2, 3, 4, 5];
+    let characters = vec!['a', 'b', 'c', 'd', 'e'];
+
+    dbg!(get_fist_integer(&numbers));
+    dbg!(get_first_character(&characters));
+}
+
+fn get_fist_integer(list: &[i32]) -> &i32 {
+    &list[0]
+}
+
+fn get_first_character(list: &[char]) -> &char {
+    &list[0]
 }
