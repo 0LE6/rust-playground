b9e3412074c755616dd9083c53e3ed07a6666474
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-17 00:15:15 +0200

    feat: example 1, using generics [T]

--- src/bin/lesson_61.rs
+++ src/bin/lesson_61.rs
@@ -6,10 +6,20 @@ fn main() {
     let numbers = vec![1, 2, 3, 4, 5];
     let characters = vec!['a', 'b', 'c', 'd', 'e'];
 
-    dbg!(get_fist_integer(&numbers));
-    dbg!(get_first_character(&characters));
+    // dbg!(get_fist_integer(&numbers));
+    // dbg!(get_first_character(&characters));
+
+    // Example 1: generics
+    dbg!(get_first(&numbers));
+    dbg!(get_first(&characters));
 }
 
+// Example 1:  use generics
+fn get_first<T>(list: &[T]) -> &T {
+    &list[0]
+}
+
+// part of the Example 0
 fn get_fist_integer(list: &[i32]) -> &i32 {
     &list[0]
 }
