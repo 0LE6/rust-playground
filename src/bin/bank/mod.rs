d67fc62c65f64e8c48510236bcfa895d887c9a71
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-17 23:14:45 +0200

    feat: add coment about mod of lesson 59

--- src/bin/bank/mod.rs
+++ src/bin/bank/mod.rs
@@ -4,3 +4,7 @@ pub mod transactions;
 pub fn announce(msg: &str) {
     println!("[BANK ACCOUNT] {msg}");
 }
+
+// https://doc.rust-lang.org/book/ch07-05-separating-modules-into-different-files.html
+// src/bin/bank.rs (seems a modern way)
+// src/bin/bank/mod.rs (older style, still supported path)
