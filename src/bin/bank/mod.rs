87b89851717eefb23d51b6acfaf8bfa30ae9fecd
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-17 23:16:36 +0200

    docs: add documentation link to rust book

--- src/bin/bank/mod.rs
+++ src/bin/bank/mod.rs
@@ -7,4 +7,4 @@ pub fn announce(msg: &str) {
 
 // https://doc.rust-lang.org/book/ch07-05-separating-modules-into-different-files.html
 // src/bin/bank.rs (seems a modern way)
-// src/bin/bank/mod.rs (older style, still supported path)
+// src/bin/bank/mod.rs (older style, stfeat: add coment about mod of lesson 59ill supported path)
