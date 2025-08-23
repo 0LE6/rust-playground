6ffdb9dc7a79921f931e826dbdd1debf3bc1d835
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-23 11:50:11 +0200

    feat: struct with different generics, T and U

--- src/bin/lesson_62.rs
+++ src/bin/lesson_62.rs
@@ -1,10 +1,14 @@
 // 62: generic structs in Rust
 
 fn main() {
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 1.5, y: 3 };
     
+    dbg!(p1, p2);
 }
 
-struct Point<T> {
+#[derive(Debug)]
+struct Point<T, U> {
     x: T,
-    y: T
+    y: U
 }
