38a4f48ecdcec0826971a393c15537bb67633627
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-25 21:15:10 +0200

    feat: return loop

--- src/bin/lesson_19.rs
+++ src/bin/lesson_19.rs
@@ -1,14 +1,28 @@
 // 19: loop
 
 fn main() {
-    let mut num = 10;
-    loop {
+    // example break
+    // let mut num = 10;
+    // loop {
+    //     println!("Let's get rusty by {num}");
+    //     num -= 1;
+    //   
+    //     if num == 0 {
+    //         println!("Loop is finished!");
+    //         break;
+    //     }
+    // }
+
+    // example return
+    let mut num = 0;
+    let result = loop {
         println!("Let's get rusty by {num}");
-        num -= 1;
+        num += 1;
       
-        if num == 0 {
-            println!("Loop is finished!");
-            break;
+        if num == 5 {
+            break "SUCCESS!";
         }
-    }
+    };
+
+    dbg!(result);
 }
