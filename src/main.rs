6279511613abbba3a999c448f107af1cb24ad8ad
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-29 23:34:55 +0200

    feat: pseudo mini grep to test arg inputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// let's try to do a mini grep example
+// extracting a word from a textfile
+
+use::std::env::args;
+
 fn main() {
-    
+
+    let args: Vec<String> = args().collect();
+    let query = &args[1];
+    let file = &args[2];
+
+    dbg!(query, file);
+
 }
