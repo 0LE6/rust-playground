17d6d722c0e6104528054244a83d2cecb21a7b92
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-08-31 22:36:07 +0200

    feat: example of generic tuple of a Point

--- src/bin/lesson_63.rs
+++ src/bin/lesson_63.rs
@@ -1,10 +1,19 @@
 // 63: generics 2
 
 fn main() {
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 1.5, y: 2.0 };
     
+    dbg!(p1.coordinates(), p2.coordinates());
 }
 
 struct Point<T> {
     x: T,
     y: T
+}
+
+impl<T> Point<T> {
+    fn coordinates(&self) -> (&T, &T) {
+        (&self.x, &self.y)        
+    }
 }
