6caa985c4d02c14a8c65d0be1ea5288d4e3bea54
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-09-12 21:42:13 +0200

    feat: update refactoring to improve modularity and error handling

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,20 @@ fn main() {
     let query = &args[1];
     let file = &args[2];
 
-    dbg!(&query, &file);
+    // dbg!(&query, &file);
 
-    let content = fs::read_to_string(&file).expect("::: There's no such file! :::'");
+    let content = fs::read_to_string(&file)
+        .expect("::: There's no such file! :::'");
 
-    dbg!(content);
+    // dbg!(content);
 
+    dbg!(parse_config(&args));
+
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
