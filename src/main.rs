3a66033f07f3d2830eb66d68b02ed4593c4f0c56
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-09-14 21:24:20 +0200

    feat: update with mproving the Error Message of no args

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,10 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!(":: Not enough arguments, mate!")
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
