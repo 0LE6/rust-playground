e1c1e421bcafaa084a86c874a88f03ad4e214883
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-09-30 22:01:57 +0200

    feat: add Creating a failing test for the search function we wish we had

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+// from Rust book
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments, mate!");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query , file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+
+    let content = fs::read_to_string(config.file_path)?;
+    
+    println!("\nWith the following text: \n {content}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three!";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));;
+    }
+}
+
+
+
+
+
 // pub mod banking {
 //     pub mod accounts {
 //         #[derive(Debug)]
@@ -65,36 +115,4 @@
 //     }
 // }
  
-
-// for Rust book
-use std::error::Error;
-use std::fs;
-
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-}
-
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments, mate!");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query , file_path })
-    }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-
-    let content = fs::read_to_string(config.file_path)?;
-    
-    println!("\nWith the following text: \n {content}");
-
-    Ok(())
-}
-
 
