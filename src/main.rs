3b2b756544a91d82d1a2d633cd1f00f20b20c5d1
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-10-01 20:48:06 +0200

    feat: update main according to the small changes from search fn (temporal)

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,23 @@ fn main() {
     let args: Vec<String> = args().collect();
 
     // using or lib.rs
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     println!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
+    //
+    // if let Err(e) = rust_playground::run(config) {
+    //     println!("Application error: {e}");
+    //     process::exit(1);
+    // }
 
-    if let Err(e) = rust_playground::run(config) {
-        println!("Application error: {e}");
-        process::exit(1);
-    }
+    let uwu = "\
+        qwerty
+        asdf
+        pepito
+        rust";
+
+    let owo = rust_playground::search("pepito", uwu);    
 
     // let args: Vec<String> = args().collect();
     // let query = &args[1];
