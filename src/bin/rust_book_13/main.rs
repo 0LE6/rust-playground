6f17c4cb94ec9bda2af19ccb6c4940ffd6c4a1bd
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-10-19 22:04:08 +0200

    feat: add ShirtColor sctruct for the beginning of the 13th lesson

--- src/bin/rust_book_13/main.rs
+++ src/bin/rust_book_13/main.rs
@@ -1,5 +1,12 @@
 // https://doc.rust-lang.org/book/ch13-00-functional-features.html#functional-language-features-iterators-and-closures
 
+// Functional Language Features: Iterators and Closures
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
 fn main() {
     println!("Hello from Rust book!"); 
 }
