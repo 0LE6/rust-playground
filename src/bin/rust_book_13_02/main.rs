2e8ca8886939f251426863992b6043fa0ddf6698
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-11-16 22:23:09 +0100

    feat: update with initial ch 13.02 example

--- src/bin/rust_book_13_02/main.rs
+++ src/bin/rust_book_13_02/main.rs
@@ -1,3 +1,10 @@
+/*
+* Processing a Series of Items with Iterators
+*/
+
 fn main() {
-    println!("13_02")
+    // Initial example
+    // https://doc.rust-lang.org/book/ch13-02-iterators.html#listing-13-10
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
 }
