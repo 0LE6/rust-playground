0427b7e9ac4dd246c19cf57225ae5ab012f5cba3
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-11-16 22:30:18 +0100

    feat: add easy iterator consumption example

--- src/bin/rust_book_13_02/main.rs
+++ src/bin/rust_book_13_02/main.rs
@@ -7,4 +7,14 @@ fn main() {
     // https://doc.rust-lang.org/book/ch13-02-iterators.html#listing-13-10
     let v1 = vec![1, 2, 3];
     let v1_iter = v1.iter();
+
+    /*
+    * iterators are lazy, meaning they have no effect until 
+    * you call methods that consume the iterator to use it up
+    */
+
+    // https://doc.rust-lang.org/book/ch13-02-iterators.html#listing-13-11
+    for val in v1_iter {
+        println!("Got: {val}");
+    }
 }
