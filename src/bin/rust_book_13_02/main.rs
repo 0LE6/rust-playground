dcd757234ddc63f69107d3e6e180d2740734857b
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-11-19 21:19:09 +0100

    feat: add test example of Calling the sum method to get the total of all items in the iterator

--- src/bin/rust_book_13_02/main.rs
+++ src/bin/rust_book_13_02/main.rs
@@ -37,4 +37,20 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(&3));
         assert_eq!(v1_iter.next(), None);
     }
+
+    /*
+    * https://doc.rust-lang.org/book/ch13-02-iterators.html#methods-that-consume-the-iterator
+    */
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
 }
+
+
