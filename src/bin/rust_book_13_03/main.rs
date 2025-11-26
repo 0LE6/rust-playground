16d20df3192b311cdb7c2d2a518084eb0822c2e6
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-11-26 19:38:55 +0100

    feat: update with Passing the return value of env::args to Config::build

--- src/bin/rust_book_13_03/main.rs
+++ src/bin/rust_book_13_03/main.rs
@@ -7,10 +7,10 @@ mod lib;
 use lib::{search, search_case_insensitive};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
