435d23d2c62d1b7558860b5c79fd04f70d75f75d
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-11-30 21:38:10 +0100

    chore: add some documentation theory

--- src/bin/rust_book_15_01/main.rs
+++ src/bin/rust_book_15_01/main.rs
@@ -1,4 +1,20 @@
 // Using Box<T> to Point to Data on the Heap
+//
+// * Boxes allow you to store data on the heap rather than the stack.
+// 
+// Usage situations:
+//
+// *    When you have a type whose size can’t be known at compile 
+//      time and you want to use a value of that type in a context 
+//      that requires an exact size
+//
+// *    When you have a large amount of data and you want to transfer 
+//      ownership but ensure the data won’t be copied when you do so
+//
+// *    When you want to own a value and you care only that 
+//      it’s a type that implements a particular trait rather than 
+//      being of a specific type
+
 fn main() {
     println!(" --- 15 ---");
 }
