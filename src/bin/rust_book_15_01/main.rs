d9c2d0a32b9c1c87c1859853fc49b608b9201cb4
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-02 00:09:17 +0100

    feat: add example of Definition of List that uses Box<T> in order to have a known size

--- src/bin/rust_book_15_01/main.rs
+++ src/bin/rust_book_15_01/main.rs
@@ -20,7 +20,9 @@ fn main() {
     // let b = Box::new(69);
     // println!("b = {b}");
 
-    let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Cons(
+        1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil)))))
+    );
 }
 
 // It will give an error because
