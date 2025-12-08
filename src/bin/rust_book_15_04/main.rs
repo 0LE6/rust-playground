b8008b2c9b11fbd093d2fc36d006e418c9dc0a84
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-08 21:56:25 +0100

    feat: add example of Printing the reference count

--- src/bin/rust_book_15_04/main.rs
+++ src/bin/rust_book_15_04/main.rs
@@ -4,6 +4,25 @@ use std::rc::Rc;
 use crate::List::{Nil, Cons};
 
 fn main() {
+    // let a = Rc::new(
+    //     Cons(
+    //         5, Rc::new(
+    //             Cons(
+    //                 10, Rc::new(Nil)
+    //             )
+    //         )
+    //     )
+    // );
+    // let b = Cons(3, Rc::clone(&a));
+    // let c = Cons(4, Rc::clone(&a));
+
+    // Rc::clone doesn’t make a deep copy
+    // of all the data like most types’ 
+    // implementations of clone do, it only 
+    // increments the reference count,
+    // which doesn’t take much time.
+
+    // Cloning an Rc<T> Increases the Reference Count 
     let a = Rc::new(
         Cons(
             5, Rc::new(
@@ -13,14 +32,27 @@ fn main() {
             )
         )
     );
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!(
+        "count after creating a = {}",
+        Rc::strong_count(&a)
+    );
 
-    // Rc::clone doesn’t make a deep copy
-    // of all the data like most types’ 
-    // implementations of clone do, it only 
-    // increments the reference count,
-    // which doesn’t take much time.
+    let b = Cons(3, Rc::clone(&a));
+    println!(
+        "count after creating b = {}",
+        Rc::strong_count(&a)
+    );
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!(
+            "count after creating c = {}",
+            Rc::strong_count(&a)
+        );
+    }
+    println!(
+        "count after c goes out of scope = {}",
+        Rc::strong_count(&a)
+    );
 }
 
 enum List {
