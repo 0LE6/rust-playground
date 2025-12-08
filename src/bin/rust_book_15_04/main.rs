b4bfac2b963a1ce9fd5487730f7bd95a58365eca
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-08 21:42:22 +0100

    feat: add exampel of A definition of List that uses Rc<T>

--- src/bin/rust_book_15_04/main.rs
+++ src/bin/rust_book_15_04/main.rs
@@ -1,16 +1,23 @@
 // Rc<T>, the Reference Counted Smart Pointer 
+
+use std::rc::Rc;
 use crate::List::{Nil, Cons};
 
 fn main() {
-    let a = Cons(
-        5, 
-        Box::new(Cons(10, Box::new(Nil)))
+    let a = Rc::new(
+        Cons(
+            5, Rc::new(
+                Cons(
+                    10, Rc::new(Nil)
+                )
+            )
+        )
     );
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
