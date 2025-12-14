ad14a8ffe26ea422fb81dc42ef92d542ac7c2fd2
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-14 23:18:36 +0100

    feat: add example of A cons list definition that holds a RefCell<T> so that we can modify what a Cons variant is referring to

--- src/bin/rust_book_15_06/main.rs
+++ src/bin/rust_book_15_06/main.rs
@@ -1,5 +1,24 @@
 // Reference Cycles Can Leak Memory
 
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(
+        &self
+    ) -> Option<&RefCell<Rc<List>>>{
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    } 
+}
+
 fn main() {
     
 }
