9867e2c0f19e594e555f037a21ffe418a3f5f250
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-15 22:00:18 +0100

    feat: add example of make the type of parent use Weak<T>

--- src/bin/rust_book_15_06/main.rs
+++ src/bin/rust_book_15_06/main.rs
@@ -1,6 +1,6 @@
 /// Reference Cycles Can Leak Memory
 
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::{Rc, Weak}};
 use crate::List::{Cons, Nil};
 
 #[derive(Debug)]
@@ -88,10 +88,15 @@ fn main() {
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
+    // the Node in leaf now has two owners: 
+    // leaf and branch
+
+
 }
 
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>,
     children: RefCell<Vec<Rc<Node>>>,
 }
