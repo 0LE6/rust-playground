d1fb3cf6b81c527ecc30fc21fc22c9bf4b6c0746
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-16 20:42:18 +0100

    feat: add example of A leaf node with a weak reference to its parent node, branch

--- src/bin/rust_book_15_06/main.rs
+++ src/bin/rust_book_15_06/main.rs
@@ -80,14 +80,27 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!(
+        "leaf parent = {:?}",
+        leaf.parent.borrow().upgrade()
+    );
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!(
+        "leaf parent = {:?}",
+        leaf.parent.borrow().upgrade()
+    );
     // the Node in leaf now has two owners: 
     // leaf and branch
 
@@ -97,6 +110,6 @@ fn main() {
 #[derive(Debug)]
 struct Node {
     value: i32,
-    parent: RefCell<Weak<Node>,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
