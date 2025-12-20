b10c4a6a5480245976b345e2c9fc52b22192a698
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-20 11:26:11 +0100

    feat: update handle.join() before the for loop in main to see what happens

--- src/bin/rust_book_16_01/main.rs
+++ src/bin/rust_book_16_01/main.rs
@@ -12,6 +12,8 @@ fn main() {
         }
     });
 
+    handle.join().unwrap(); // JoinHandle<()>
+
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_secs(1));
@@ -19,7 +21,7 @@ fn main() {
 
     // join makes sure the spawned thread 
     // finishes before main exits.   
-    handle.join().unwrap(); // JoinHandle<()>
+    // handle.join().unwrap(); // JoinHandle<()>
     // main thread waits because of the
     // call to handle.join() and does not 
     // end until the spawned thread is finished.
