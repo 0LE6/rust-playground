e9523970e7f52ffca5aae6d776cca360d7e053f2
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-20 11:15:55 +0100

    feat: update example to get the same output as in the book

--- src/bin/rust_book_16_01/main.rs
+++ src/bin/rust_book_16_01/main.rs
@@ -6,14 +6,14 @@ fn main() {
     thread::spawn(|| {
         for i in 1..10 {
             println!(
-                "hi number {i} from the spawned thread"
+                "hi number {i} from the spawned thread!"
             );
-            thread::sleep(Duration::from_millis(1));
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
     for i in 1..5 {
-        println!("hi number {i} from the main thread");
-        thread::sleep(Duration::from_millis(1));
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_secs(1));
     }
 }
