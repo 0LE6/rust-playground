a130ebcc74cd508f4f7a5812ca38cc79543c3211
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-20 11:33:01 +0100

    feat: add example of Attempting to use a vector created by the main thread in another thread

--- src/bin/rust_book_16_01/main.rs
+++ src/bin/rust_book_16_01/main.rs
@@ -3,21 +3,21 @@
 use std::{thread, time::Duration};
 
 fn main() { 
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!(
-                "hi number {i} from the spawned thread!"
-            );
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    handle.join().unwrap(); // JoinHandle<()>
-
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_secs(1));
-    }
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!(
+    //             "hi number {i} from the spawned thread!"
+    //         );
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+    //
+    // handle.join().unwrap(); // JoinHandle<()>
+    //
+    // for i in 1..5 {
+    //     println!("hi number {i} from the main thread!");
+    //     thread::sleep(Duration::from_secs(1));
+    // }
 
     // join makes sure the spawned thread 
     // finishes before main exits.   
@@ -25,4 +25,15 @@ fn main() {
     // main thread waits because of the
     // call to handle.join() and does not 
     // end until the spawned thread is finished.
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(|| {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
+
+
+
 }
