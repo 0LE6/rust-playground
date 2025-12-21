af6dfdda1b6cbe6315de5e17025ce511e0f284c8
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-21 21:19:10 +0100

    feat: update lesson with default impl usage of a Trait

--- src/bin/lesson_64.rs
+++ src/bin/lesson_64.rs
@@ -1,65 +1,103 @@
 // 64: Traits in Rust 
 
+use std::{io::Stdin, path::StripPrefixError};
+
 pub trait Summary {
-    fn summarize(&self) -> String;
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-pub struct SocialPost {
-    pub username: String,
-    pub content: String,
-    pub replay: bool,
-    pub repost: bool,
-}
-
-impl Summary for NewsArticle {
     fn summarize(&self) -> String {
-        format!(
-            "{}, by {} ({})",
-            self.headline,
-            self.author,
-            self.location
-        )
+        String::from("(Read more...)")
     }
 }
 
-impl Summary for SocialPost {
+// pub struct NewsArticle {
+//     pub headline: String,
+//     pub location: String,
+//     pub author: String,
+//     pub content: String,
+// }
+//
+// pub struct SocialPost {
+//     pub username: String,
+//     pub content: String,
+//     pub replay: bool,
+//     pub repost: bool,
+// }
+//
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!(
+//             "{}, by {} ({})",
+//             self.headline,
+//             self.author,
+//             self.location
+//         )
+//     }
+// }
+//
+// impl Summary for SocialPost {
+//     fn summarize(&self) -> String {
+//         format!(
+//             "{} : {}",
+//             self.username,
+//             self.content,
+//         )
+//     }
+// }
+
+struct SimpleNote {
+    content: String,
+}
+
+struct DetailedPost {
+    username: String, 
+    content: String,
+}
+
+impl Summary for SimpleNote {}
+
+impl Summary for DetailedPost {
     fn summarize(&self) -> String {
         format!(
             "{} : {}",
             self.username,
-            self.content,
+            self.content
         )
     }
 }
-
 fn main() {
-    let article = NewsArticle {
-        headline: String::from(
-            "Cacao for cocoa!"
-        ),
-        location: String::from("Cocoa Island"),
-        author: String::from("Coco Channel"),
+    let note = SimpleNote {
         content: String::from(
-            "Marvelous coco experience!"
-        )
-    }; 
-
-    let post = SocialPost {
-        username: String::from(
-            "Cacao magazine"
+            "This is a simple note"
         ),
-        content: String::from("Cocoa Island"),
-        replay: false,
-        repost: false,
     };
 
-    println!("{}", article.summarize());
-    println!("{}", post.summarize());
+    let post = DetailedPost {
+        username: String::from("user123"),
+        content: String::from("Hello, world!"),
+    };
+
+    println!("Note {}", note.summarize());
+    println!("Note {}", post.summarize());
+
+    // let article = NewsArticle {
+    //     headline: String::from(
+    //         "Cacao for cocoa!"
+    //     ),
+    //     location: String::from("Cocoa Island"),
+    //     author: String::from("Coco Channel"),
+    //     content: String::from(
+    //         "Marvelous coco experience!"
+    //     )
+    // }; 
+    //
+    // let post = SocialPost {
+    //     username: String::from(
+    //         "Cacao magazine"
+    //     ),
+    //     content: String::from("Cocoa Island"),
+    //     replay: false,
+    //     repost: false,
+    // };
+    //
+    // println!("{}", article.summarize());
+    // println!("{}", post.summarize());
 }
