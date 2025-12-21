cb0687c37ceb63721e7fc2691f7c285bdde9d449
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-21 19:40:39 +0100

    feat: add example of Moving tx to a spawned thread and sending "Hi"

--- src/bin/rust_book_16_02/main.rs
+++ src/bin/rust_book_16_02/main.rs
@@ -1,7 +1,14 @@
 // Transfer Data Between Threads with Message Passing
 
-use std::sync::mpsc;
+use std::{sync::mpsc, thread};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+    
+    thread::spawn(move || {
+        let val = String::from("Hi");
+        tx.send(val).unwrap();
+    });
+
 }
+
