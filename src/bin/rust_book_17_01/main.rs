c97cd68a902e5adc4ca291bd29ef62b015ad71f5
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-22 20:05:30 +0100

    feat: update example Chaining with the await keyword

--- src/bin/rust_book_17_01/main.rs
+++ src/bin/rust_book_17_01/main.rs
@@ -9,9 +9,12 @@ fn main() {
 async fn page_title(
     url: &str
 ) -> Option<String> {
-    let response = trpl::get(url).await;
-    let response_text = response.text().await;
-    Html::parse(&response_text)
+    let response = trpl::get(url)
+        .await
+        .text()
+        .await;
+    // let response_text = response.text().await;
+    Html::parse(&response)
         .select_first("title")
         .map(|title| title.inner_html())
 }
