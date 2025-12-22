7ffecbd12bc265fa7eee30a12bc58fb6531c5256
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-22 18:52:14 +0100

    feat: update example trying Ten threads, each incrementing a counter guarded by a Mutex<T>

--- src/bin/rust_book_16_03/main.rs
+++ src/bin/rust_book_16_03/main.rs
@@ -1,14 +1,39 @@
 // Shared-State Concurrency
 
-use std::sync::Mutex;
+use std::{sync::Mutex, thread};
 
 fn main() {
-    let m = Mutex::new(5);
+    // let m = Mutex::new(5);
+    //
+    // {
+    //     let mut num = m.lock().unwrap();
+    //     *num = 6;
+    // }
+    //
+    // println!("m = {m:?}");
 
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+
+    for _ in 1..10 {
+        let handle = thread::spawn(
+            move || {
+                let mut num = counter
+                    .lock()
+                    .unwrap();
+
+                *num += 1;
+            }
+        );
+        handles.push(handle);
     }
 
-    println!("m = {m:?}");
+    for hanlde in handles {
+        hanlde.join().unwrap();       
+    }
+
+    println!(
+        "Result: {}",
+        *counter.lock().unwrap()
+    );
 }
