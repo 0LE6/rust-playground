b481dfdd4175369c627bdf88f9676a9d7549fc25
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-22 19:53:20 +0100

    feat: add example of Defining an async function to get the title element from an HTML page

---
+++ src/bin/rust_book_17_01/main.rs
@@ -0,0 +1,17 @@
+// Futures and the Async Syntax
+
+use trpl::Html;
+
+fn main() {
+    
+}
+
+async fn page_title(
+    url: &str
+) -> Option<String> {
+    let response = trpl::get(url).await;
+    let response_text = response.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title| title.inner_html())
+}
