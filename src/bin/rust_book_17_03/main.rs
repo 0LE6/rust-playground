311d38d7e106a60866e8755c7a2006d2977f5cf8
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-26 23:00:19 +0100

    feat: update example Using our imagined timeout to run a slow operation with a time limit

--- src/bin/rust_book_17_03/main.rs
+++ src/bin/rust_book_17_03/main.rs
@@ -5,32 +5,51 @@ use std::{thread, time::Duration};
 fn main() {
     trpl::block_on(async {
         // let one_ms = Duration::from_millis(1);
-        let a = async {
-            println!("'a' started");
-            slow("a", 30);
-            trpl::yield_now().await;
-            slow("a", 10);
-            trpl::yield_now().await;
-            slow("a", 20);
-            trpl::yield_now().await;
-            println!("'a' finished!");
+        // let a = async {
+        //     println!("'a' started");
+        //     slow("a", 30);
+        //     trpl::yield_now().await;
+        //     slow("a", 10);
+        //     trpl::yield_now().await;
+        //     slow("a", 20);
+        //     trpl::yield_now().await;
+        //     println!("'a' finished!");
+        // };
+        //
+        // let b = async {
+        //     println!("'b' started");
+        //     slow("b", 75);
+        //     trpl::yield_now().await;
+        //     slow("b", 10);
+        //     trpl::yield_now().await;
+        //     slow("b", 15);
+        //     trpl::yield_now().await;
+        //     slow("b", 350);
+        //     trpl::yield_now().await;
+        //     println!("'b' finished!");
+        // };
+        //
+        // trpl::select(a, b).await;
+
+        let slow = async {
+            trpl::sleep(
+                Duration::from_secs(5)
+            ).await;
+            "Finally finished!"
         };
 
-        let b = async {
-            println!("'b' started");
-            slow("b", 75);
-            trpl::yield_now().await;
-            slow("b", 10);
-            trpl::yield_now().await;
-            slow("b", 15);
-            trpl::yield_now().await;
-            slow("b", 350);
-            trpl::yield_now().await;
-            println!("'b' finished!");
-        };
-
-        trpl::select(a, b).await;
-
+        match timeout(
+            slow,
+            Duration::from_secs(2)
+        ).await {
+            Ok(msg) => println!("OK with '{msg}'"),
+            Err(duration) => {
+                println!(
+                    "FAIL after {} secs", 
+                    duration.as_secs()
+                );          
+            }
+        }
     });
 }
 
