f675b44e8179e5bf3f2d3442f352829e5ab8c1a2
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-26 20:48:44 +0100

    feat: update example Creating an async channel and assigning the two halves to tx and rx

--- src/bin/rust_book_17_02/main.rs
+++ src/bin/rust_book_17_02/main.rs
@@ -4,28 +4,36 @@ use std::time::Duration;
 
 fn main() {
     trpl::block_on(async {
-        let fut1 = async {
-            for i in 1..10 {
-                println!(
-                    "Hi number {i} from the 1st task!"
-                );
-                trpl::sleep(
-                    Duration::from_millis(500)
-                ).await
-            }
-        };
+        // let fut1 = async {
+        //     for i in 1..10 {
+        //         println!(
+        //             "Hi number {i} from the 1st task!"
+        //         );
+        //         trpl::sleep(
+        //             Duration::from_millis(500)
+        //         ).await
+        //     }
+        // };
+        //
+        // let fut2 = async {
+        //     for i in 1..5 {
+        //         println!(
+        //             "Hi number {i} from the 2nd task!"
+        //         );            
+        //         trpl::sleep(
+        //             Duration::from_millis(500)
+        //         ).await
+        //     }
+        // };
+        //
+        // trpl::join(fut1, fut2).await;
 
-        let fut2 = async {
-            for i in 1..5 {
-                println!(
-                    "Hi number {i} from the 2nd task!"
-                );            
-                trpl::sleep(
-                    Duration::from_millis(500)
-                ).await
-            }
-        };
+        let (tx, mut rx) = trpl::channel();
 
-        trpl::join(fut1, fut2).await;
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+
+        let received = rx.recv().await.unwrap();
+        println!("received '{received}'");
     });
 }
