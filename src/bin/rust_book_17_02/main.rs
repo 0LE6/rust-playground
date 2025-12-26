ed43dce19192b4706fcaa20885a996e008235570
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-26 20:54:32 +0100

    feat: update example Sending and receiving multiple messages over the async channel and sleeping with an await between each message

--- src/bin/rust_book_17_02/main.rs
+++ src/bin/rust_book_17_02/main.rs
@@ -30,10 +30,28 @@ fn main() {
 
         let (tx, mut rx) = trpl::channel();
 
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        // let val = String::from("hi");
+        // tx.send(val).unwrap();
+        //
+        // let received = rx.recv().await.unwrap();
+        // println!("received '{received}'");
 
-        let received = rx.recv().await.unwrap();
-        println!("received '{received}'");
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("future"),
+        ];
+
+        for val in vals {
+           tx.send(val).unwrap();
+           trpl::sleep(
+               Duration::from_millis(500)
+           ).await
+        }
+
+        while let Some(value) = rx.recv().await {
+            println!("received '{value}'");
+        }
     });
 }
