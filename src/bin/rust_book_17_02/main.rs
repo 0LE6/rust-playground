d239812d67b7a4510c26db14c0eca745903fd002
0LE6 <135649528+0LE6@users.noreply.github.com>
2025-12-26 20:43:59 +0100

    feat: update example Using trpl::join to await two anonymous futures

--- src/bin/rust_book_17_02/main.rs
+++ src/bin/rust_book_17_02/main.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 fn main() {
     trpl::block_on(async {
-        let handle = trpl::spawn_task(async {
+        let fut1 = async {
             for i in 1..10 {
                 println!(
                     "Hi number {i} from the 1st task!"
@@ -13,17 +13,19 @@ fn main() {
                     Duration::from_millis(500)
                 ).await
             }
-        });
+        };
 
-        for i in 1..5 {
-            println!(
-                "Hi number {i} from the 2nd task!"
-            );            
-            trpl::sleep(
-                Duration::from_millis(500)
-            ).await
-        }
+        let fut2 = async {
+            for i in 1..5 {
+                println!(
+                    "Hi number {i} from the 2nd task!"
+                );            
+                trpl::sleep(
+                    Duration::from_millis(500)
+                ).await
+            }
+        };
 
-        handle.await.unwrap();
+        trpl::join(fut1, fut2).await;
     });
 }
