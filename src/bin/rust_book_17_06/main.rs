81b69fe6a30aaa669f110c43b378cc61af436fdb
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-02 20:41:59 +0100

    feat: add example of Sending messages with blocking code in a thread and awaiting the messages in an async block

---
+++ src/bin/rust_book_17_06/main.rs
@@ -0,0 +1,23 @@
+// Putting It All Together: 
+// Futures, Tasks and Threads
+
+use std::{thread, time::Duration};
+
+fn main() {
+    let (tx, mut rx) = trpl::channel();
+
+    thread::spawn(move || {
+        for i in 1..11 {
+            tx.send(i).unwrap();
+            thread::sleep(
+                Duration::from_secs(1)
+            );
+        }
+    });
+
+    trpl::block_on(async {
+        while let Some(message) = rx.recv().await {
+            println!("{message}");
+        }
+    });
+}
