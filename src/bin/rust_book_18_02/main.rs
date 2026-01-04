5dc738a93f2ae2742492a5598cef2372fe11c414
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-04 23:27:41 +0100

    feat: update example attempting to use a type String, that doesn't implement the trait object's trait (the trait `Draw` is not implemented for `String`)

--- src/bin/rust_book_18_02/main.rs
+++ src/bin/rust_book_18_02/main.rs
@@ -17,24 +17,30 @@ impl Draw for SelectBox {
 }
 
 fn main() {
+    // let screen = Screen {
+    //     components: vec![
+    //         Box::new(SelectBox {
+    //             width: 75,
+    //             height: 10,
+    //             options: vec![
+    //                 String::from("Yes"),
+    //                 String::from("Maybe"),
+    //                 String::from("No"),
+    //             ],
+    //         }),
+    //         Box::new(Button {
+    //             width: 50,
+    //             height: 10,
+    //             label: String::from("OK"),
+    //         }),
+    //     ]
+    // }; 
+
     let screen = Screen {
         components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ]
-    }; 
+            Box::new(String::from("Hi"))
+        ],
+    };
 
     screen.run();
 }
