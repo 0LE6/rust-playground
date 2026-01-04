8267a272642b766a5047a91175ef3e0d06113fdf
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-04 23:13:19 +0100

    feat: update example with Another crate using our lib and implementing the Draw trait on a SelectBox struct

--- src/bin/rust_book_18_02/main.rs
+++ src/bin/rust_book_18_02/main.rs
@@ -2,6 +2,19 @@
 // over Shared Behavior
 
 mod lib;
+use lib::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code
+    }
+}
 
 fn main() {
         
