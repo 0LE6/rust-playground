12235922010191bd55f7c5a9129e66a16bd13c2a
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-17 22:38:14 +0100

    feat: add example Mixing if let, else if, else if let, and else

---
+++ src/bin/rust_book_19_01/main.rs
@@ -0,0 +1,25 @@
+// All the Places Patterns Can Be Used
+
+fn main() {
+    let fav_color: Option<&str> = None;
+
+    let is_tuesday: bool = false;
+
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = fav_color {
+        println!(
+            "Using your fav color, {color}, as bg"
+        );
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as bg color");
+        } else {
+            println!("Using orange as bg color");
+        }
+    } else {
+        println!("Using blue as bg color");
+    }
+}
