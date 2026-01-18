092a7b2a0ba40c1a0fed454fc5fe2bd5824c3400
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-18 12:58:59 +0100

    feat: add example Using a while let loop to print values for as long as rx.recv() returns Ok

--- src/bin/rust_book_19_01/main.rs
+++ src/bin/rust_book_19_01/main.rs
@@ -1,25 +1,36 @@
 // All the Places Patterns Can Be Used
 
 fn main() {
-    let fav_color: Option<&str> = Some("banana");
+    // let fav_color: Option<&str> = Some("banana");
+    //
+    // let is_tuesday: bool = false;
+    //
+    // let age: Result<u8, _> = "34".parse();
+    //
+    // if let Some(color) = fav_color {
+    //     println!(
+    //         "Using your fav color, {color}, as bg"
+    //     );
+    // } else if is_tuesday {
+    //     println!("Tuesday is green day!");
+    // } else if let Ok(age) = age {
+    //     if age > 30 {
+    //         println!("Using purple as bg color");
+    //     } else {
+    //         println!("Using orange as bg color");
+    //     }
+    // } else {
+    //     println!("Using blue as bg color");
+    // }
 
-    let is_tuesday: bool = false;
-
-    let age: Result<u8, _> = "34".parse();
-
-    if let Some(color) = fav_color {
-        println!(
-            "Using your fav color, {color}, as bg"
-        );
-    } else if is_tuesday {
-        println!("Tuesday is green day!");
-    } else if let Ok(age) = age {
-        if age > 30 {
-            println!("Using purple as bg color");
-        } else {
-            println!("Using orange as bg color");
+    let (tx, rx) = std::sync::mpsc::channel();
+    std::thread::spawn(move || {
+        for val in [1, 2, 3] {
+            tx.send(val).unwrap();
         }
-    } else {
-        println!("Using blue as bg color");
+    });
+
+    while let Ok(value) = rx.recv() {
+        println!("{value}");
     }
 }
