a48e66d33c7988143339bc299304edbdbc0b4f61
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-19 20:43:34 +0100

    feat: add example Attempting to use an irrefutable patterns with let...else

--- src/bin/rust_book_19_02/main.rs
+++ src/bin/rust_book_19_02/main.rs
@@ -2,6 +2,10 @@
 // Pattern Might Fail to Match
 
 fn main() {
-    let some_option_value: Option<i32> = None;
-    let Some(x) = some_option_value else { return; };
+    // let some_option_value: Option<i32> = None;
+    // let Some(x) = some_option_value else { return; };
+
+    let x = 5 else {
+        return;
+    };
 }
