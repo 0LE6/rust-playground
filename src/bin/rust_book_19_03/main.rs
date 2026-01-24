b99f853996345f2b9cdae1336b28214f813a8410
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-24 18:47:39 +0100

    feat: add example combinigin multiple patterns with a match guard

--- src/bin/rust_book_19_03/main.rs
+++ src/bin/rust_book_19_03/main.rs
@@ -159,16 +159,26 @@ fn main() {
     // }
 
     // let x = Some(5);
-    let x = Some(10);
-    let y = 10;
+    // let x = Some(10);
+    // let y = 10;
+    //
+    // match x {
+    //     Some(50) => println!("Got 50"),
+    //     Some(n) if n == y => println!("Matched, n = {n}"),
+    //     _ => println!("Default case, x = {x:?}"),
+    // }
+    //
+    // println!("at the end: x = {x:?} & y = {y}");
+
+    let x = 4;
+    // let y = false;
+    let y = true;
 
     match x {
-        Some(50) => println!("Got 50"),
-        Some(n) if n == y => println!("Matched, n = {n}"),
-        _ => println!("Default case, x = {x:?}"),
+        4 | 5 | 6 if y => println!("YES"),
+        _ => println!("NO"),
     }
 
-    println!("at the end: x = {x:?} & y = {y}");
 }
 
 fn foo(_: i32, y: i32) {
