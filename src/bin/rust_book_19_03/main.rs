02ed8c9ecafeee7ae3faea89ad40f7580db79394
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-24 18:35:46 +0100

    feat: add example matching only the first and last values in a tuple and igrnoring all other values

--- src/bin/rust_book_19_03/main.rs
+++ src/bin/rust_book_19_03/main.rs
@@ -135,10 +135,18 @@ fn main() {
     //     }
     // }
 
-    let origin = Point { x: 0, y: 0, z: 0 };
+    // let origin = Point { x: 0, y: 0, z: 0 };
+    //
+    // match origin {
+    //     Point { x, .. } => println!("x is {x}"),        
+    // }
 
-    match origin {
-        Point { x, .. } => println!("x is {x}"),        
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }        
     }
 }
 
