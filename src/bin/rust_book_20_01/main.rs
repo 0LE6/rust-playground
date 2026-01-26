f643a25cc2a6eacb6c86558fb8e35913362354c5
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-26 20:14:55 +0100

    feat: add example using the safe split_at_mut fn

--- src/bin/rust_book_20_01/main.rs
+++ src/bin/rust_book_20_01/main.rs
@@ -1,21 +1,30 @@
 // Unsafe Rust
 
 fn main() {
-    let mut num = 5;
+    // let mut num = 5;
+    //
+    // let r1 = &raw const num;
+    // let r2 = &raw mut num;
+    //
+    // unsafe {
+    //     println!("{} - {}", *r1, *r2);
+    // }
+    //
+    // let address = 0x012345usize;
+    // let r = address as *const i32;
+    //
+    // unsafe {
+    //     dangerous();
+    // }
 
-    let r1 = &raw const num;
-    let r2 = &raw mut num;
+    let mut v = vec![1, 2, 3, 4, 5, 6];
 
-    unsafe {
-        println!("{} - {}", *r1, *r2);
-    }
+    let r = &mut v[..];
 
-    let address = 0x012345usize;
-    let r = address as *const i32;
+    let (a, b) = r.split_at_mut(3);
 
-    unsafe {
-        dangerous();
-    }
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 }
 
 unsafe fn dangerous() { }
