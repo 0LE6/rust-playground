49563f89c53c3818a384af8d96f48b2b7fcc1db7
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-26 20:33:01 +0100

    feat: add example creating a slice from an arbitrary memory location

--- src/bin/rust_book_20_01/main.rs
+++ src/bin/rust_book_20_01/main.rs
@@ -19,14 +19,23 @@ fn main() {
     //     dangerous();
     // }
 
-    let mut v = vec![1, 2, 3, 4, 5, 6];
+    // let mut v = vec![1, 2, 3, 4, 5, 6];
+    //
+    // let r = &mut v[..];
+    //
+    // let (a, b) = r.split_at_mut(3);
+    //
+    // assert_eq!(a, &mut [1, 2, 3]);
+    // assert_eq!(b, &mut [4, 5, 6]);
 
-    let r = &mut v[..];
+    let address = 0x012345usize;
+    let r = address as *mut i32;
+    
+    let values: &[i32] =
+        unsafe {
+            slice::from_raw_parts_mut(r, 10000)
+        };
 
-    let (a, b) = r.split_at_mut(3);
-
-    assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5, 6]);
 }
 
 fn split_at_mut(
