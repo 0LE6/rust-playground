016c1bedcdd67cf070cdf9326c08875d2b8cd69e
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-26 20:52:07 +0100

    chore: it is idiomatic to write a comment starting SAFETY and explaining what the caller needs to do to call the fn safely

--- src/bin/rust_book_20_01/main.rs
+++ src/bin/rust_book_20_01/main.rs
@@ -56,12 +56,16 @@ fn main() {
     // println!("value is: {HELLO_WORLD}");
     
     unsafe {
+        // SAFETY: This is only called from a single thread in `main`.
         add_to_counter(3);
         println!("COUNTER: {}", *(&raw const COUNTER));
     }
 
 }
 
+/// SAFETY: Calling this from more than a single thread at a time is undefined
+/// behavior, so you *must* guarantee you only call it from a single thread at
+/// a time.
 unsafe fn add_to_counter(inc: u32) {
     unsafe {
         COUNTER += inc;
