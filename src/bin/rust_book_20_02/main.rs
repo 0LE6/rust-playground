22091c2c0b2c9637d1367ce9c91d467fba25b710
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-29 19:56:59 +0100

    chore: testing output_print on a Point that impl Display that impl OutlinePrint (supertrait)

--- src/bin/rust_book_20_02/main.rs
+++ src/bin/rust_book_20_02/main.rs
@@ -34,7 +34,17 @@ fn main() {
     // <Type as Trait>::function(receiver_if_method, next_arg, ...);
 
 
+    let point = Point { x: 1, y: 2 };
+    point.outline_print();
 }
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl OutlinePrint for Point {}
 
 trait OutlinePrint: fmt::Display {
     fn outline_print(&self) {
@@ -42,9 +52,9 @@ trait OutlinePrint: fmt::Display {
         let len = output.len();
 
         println!("{}", "*".repeat(len + 4));
-        println!("*{}", " ".repeat(len + 2));
+        println!("*{}*", " ".repeat(len + 2));
         println!("* {output} *");
-        println!("{}", " ".repeat(len + 2));
+        println!("*{}*", " ".repeat(len + 2));
         println!("{}", "*".repeat(len + 4));
     }
 }
