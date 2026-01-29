a8427133c21d59ba612696c3f69de7acdb9daaff
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-29 19:00:49 +0100

    feat: add example of implementing the Add trait to overload the + operant for Point instances

--- src/bin/rust_book_20_02/main.rs
+++ src/bin/rust_book_20_02/main.rs
@@ -1,31 +1,53 @@
 /* Advanced Trais */
 
+use std::ops::Add;
+
 fn main() {
-    
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
 }
 
-struct Counter {
-    count: u32,
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-impl Counter {
-    fn new() -> Counter {
-        Counter { count: 0 }
-    }
-}
+impl Add for Point {
+    type Output = Point;    
 
-impl Iterator for Counter {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < 5 {
-            self.count += 1;
-            Some(self.count)
-        } else {
-            None
+    fn add(self, other: Point) -> Point {
+        Point { 
+            x: self.x + other.x,
+            y: self.y + other.y, 
         }
     }
 }
+
+// struct Counter {
+//     count: u32,
+// }
+//
+// impl Counter {
+//     fn new() -> Counter {
+//         Counter { count: 0 }
+//     }
+// }
+//
+// impl Iterator for Counter {
+//     type Item = u32;
+//
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if self.count < 5 {
+//             self.count += 1;
+//             Some(self.count)
+//         } else {
+//             None
+//         }
+//     }
+// }
 
 // an hypothetical definition
 // of a trait using generics
