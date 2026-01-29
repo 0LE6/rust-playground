c767128924b0b82f9a2c4a1dd1da1f58016d27eb
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-29 20:03:29 +0100

    feat: add example creating a Wrapper type around Vec<String> to impo Display

--- src/bin/rust_book_20_02/main.rs
+++ src/bin/rust_book_20_02/main.rs
@@ -36,6 +36,23 @@ fn main() {
 
     let point = Point { x: 1, y: 2 };
     point.outline_print();
+
+    let w = Wrapper(
+        vec![
+        String::from("hello"),
+        String::from("world")
+        ]
+    );
+    println!("w = {w}");
+
+}
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
 }
 
 impl fmt::Display for Point {
