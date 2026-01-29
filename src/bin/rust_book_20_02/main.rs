9c32385bc73c68690bd9b40d7cd7e8682079b247
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-01-29 19:25:55 +0100

    feat: add example of a trait with an associated fn and a type with an associated fn of the same name that also impl the trait

--- src/bin/rust_book_20_02/main.rs
+++ src/bin/rust_book_20_02/main.rs
@@ -3,15 +3,15 @@
 use std::ops::Add;
 
 fn main() {
-    assert_eq!(
-        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-        Point { x: 3, y: 3 }
-    );
-
-    assert_eq!(
-        Millimeters( 2000) + Meters(2),
-        Millimeters(4000)
-    );
+    // assert_eq!(
+    //     Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+    //     Point { x: 3, y: 3 }
+    // );
+    //
+    // assert_eq!(
+    //     Millimeters( 2000) + Meters(2),
+    //     Millimeters(4000)
+    // );
 
     /* In a fail situation
 
@@ -20,71 +20,91 @@ fn main() {
     right: Millimeters(4001)
     */
 
-    let person = Human;
-    Pilot::fly(&person);
-    Wizard::fly(&person);
-    person.fly(); // *waving arms furiously*
+    // let person = Human;
+    // Pilot::fly(&person);
+    // Wizard::fly(&person);
+    // person.fly(); // *waving arms furiously*
+
+    println!("A baby dog is called a {}", Dog::baby_name());
 
 }
 
-trait Pilot {
-    fn fly(&self);
+trait Animal {
+    fn baby_name() -> String;
 }
 
-trait Wizard {
-    fn fly(&self);
-}
+struct Dog;
 
-struct Human;
-
-impl Pilot for Human {
-    fn fly(&self) {
-        println!("This is your captain speaking."); 
-    }   
-}
-
-impl Wizard for Human {
-    fn fly(&self) {
-        println!("Fly, you fools!");
-    }   
-}
-
-impl Human {
-    fn fly(&self) {
-        println!("*waving arms furiously*");
-    }   
-}
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-struct Millimeters(u32);
-#[derive(Debug, Copy, Clone, PartialEq)]
-struct Meters(u32);
-
-impl Add<Meters> for Millimeters {
-    type Output = Millimeters;
-
-    fn add(self, other: Meters) -> Millimeters {
-        Millimeters(self.0 + (other.0 * 1000))
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Doggy")
     }
 }
 
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl Add for Point {
-    type Output = Point;    
-
-    fn add(self, other: Point) -> Point {
-        Point { 
-            x: self.x + other.x,
-            y: self.y + other.y, 
-        }
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
+
+// trait Pilot {
+//     fn fly(&self);
+// }
+//
+// trait Wizard {
+//     fn fly(&self);
+// }
+//
+// struct Human;
+//
+// impl Pilot for Human {
+//     fn fly(&self) {
+//         println!("This is your captain speaking."); 
+//     }   
+// }
+//
+// impl Wizard for Human {
+//     fn fly(&self) {
+//         println!("Fly, you fools!");
+//     }   
+// }
+//
+// impl Human {
+//     fn fly(&self) {
+//         println!("*waving arms furiously*");
+//     }   
+// }
+//
+// #[derive(Debug, Copy, Clone, PartialEq)]
+// struct Millimeters(u32);
+// #[derive(Debug, Copy, Clone, PartialEq)]
+// struct Meters(u32);
+//
+// impl Add<Meters> for Millimeters {
+//     type Output = Millimeters;
+//
+//     fn add(self, other: Meters) -> Millimeters {
+//         Millimeters(self.0 + (other.0 * 1000))
+//     }
+// }
+//
+//
+// #[derive(Debug, Copy, Clone, PartialEq)]
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+//
+// impl Add for Point {
+//     type Output = Point;    
+//
+//     fn add(self, other: Point) -> Point {
+//         Point { 
+//             x: self.x + other.x,
+//             y: self.y + other.y, 
+//         }
+//     }
+// }
 
 // struct Counter {
 //     count: u32,
