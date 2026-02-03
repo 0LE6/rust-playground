18dc4e7843409360e12c672634bb5e9bc26fe158
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-02-03 19:49:39 +0100

    feat: add example using a closure with the map method to convet nums to strs

--- src/bin/rust_book_20_04/main.rs
+++ src/bin/rust_book_20_04/main.rs
@@ -1,15 +1,27 @@
 // Advanced Functions and Closures
 
 fn main() {
-    let ans = do_twice(add_one, 5);   
+    // let ans = do_twice(add_one, 5);   
+    //
+    // println!("The answer is: {ans}");
 
-    println!("The answer is: {ans}");
+    let list_of_nums: Vec<i32> = Vec::from([1, 2, 3]);
+    // let list_of_nums = vec![1, 2, 3];
+    let list_of_strs: Vec<String> = list_of_nums
+        .iter()
+        .map(|n| n.to_string())
+        .collect();
+
+    println!("nums {:?}", list_of_nums);
+    println!("strs {:?}", list_of_strs);
+
+
 }
 
-fn add_one(x: i32) -> i32 {
-    x + 1    
-}
-
-fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
-    f(arg) + f(arg)
-}
+// fn add_one(x: i32) -> i32 {
+//     x + 1    
+// }
+//
+// fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+//     f(arg) + f(arg)
+// }
