4d7d80a50da2a733e4093f02568e7b310470efd1
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-02-06 21:47:17 +0100

    chore: clean for final project but comment all previous made

--- src/lib.rs
+++ src/lib.rs
@@ -1,170 +1,199 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------------
+
+
 // from Rust book
-use std::error::Error;
-use std::fs;
-
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-}
-
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments, mate!");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query , file_path })
-    }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-
-    let content = fs::read_to_string(config.file_path)?;
-    
-    println!("\nWith the following text: \n {content}");
-
-    Ok(())
-}
-
-// example for not use Vec, faster and the same understanding
-// pub fn search<'a>(query: &str, contents: &'a str) {/* -> Vec<&'a str> { */
-
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-            println!("Eureka! It worked! :D\n");
-            println!("fn search()\n");
-        }
-    }
-
-    result
-}
-
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str
-) -> Vec<&'a str> {
-
-    let query = query.to_lowercase();
-    
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-            println!("fn search_case_insensitive()\n");
-        }
-    }
-
-    result
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three
-Duct tape.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "RuSt";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
-    }
-}
-
-
-
-
-
-// pub mod banking {
-//     pub mod accounts {
-//         #[derive(Debug)]
-//         pub struct Account {
-//             pub account_number: u32,
-//             pub balance: f64,
+// use std::error::Error;
+// use std::fs;
+//
+// pub struct Config {
+//     pub query: String,
+//     pub file_path: String,
+// }
+//
+// impl Config {
+//     pub fn build(args: &[String]) -> Result<Config, &'static str> {
+//         if args.len() < 3 {
+//             return Err("Not enough arguments, mate!");
 //         }
 //
-//         pub fn open_account(id: u32) -> Account {
-//             println!("Account {} opened!", id);
-//             Account { account_number: (id), balance: (0.0) }
-//         }
+//         let query = args[1].clone();
+//         let file_path = args[2].clone();
 //
-//         #[allow(dead_code)]
-//         fn close_account(account: &mut Account) {
-//             println!("Account {} closed!", account.account_number);
-//             account.balance = 0.0;
-//         }
-//     }
-//
-//     pub mod transactions {
-//         // super allows us accese to functionality outside
-//         // the "transactions" module
-//         use super::accounts::Account;
-//
-//         pub fn deposit(account: &mut Account, amount: f64) {
-//             account.balance += amount;
-//             println!(
-//                 "Deposited ${:.2} into Account {}. New balance: {:.2}",
-//                 amount, account.account_number, account.balance
-//             );
-//         }
-//
-//         pub fn withdraw(acc: &mut Account, amount: f64) {
-//             if amount > acc.balance {
-//                 println!(
-//                     "[TRANSACTION] ERROR: Insufficient funds for ${} withdraw from account 
-//                     {} with balance ${:.2}",
-//                     amount, acc.account_number, acc.balance
-//                 );
-//             } else {
-//                 acc.balance -= amount;
-//                 println!(
-//                     "[TRANSACTION] Withdraw ${:.2} from Account {}. New balance: ${:.2}",
-//                     amount, acc.account_number, acc.balance
-//                 );
-//             }
-//         }
-//
-//         pub fn transfer(from: &mut Account, to: &mut Account, amount: f64) {
-//             if from.balance >= amount {
-//                 from.balance -= amount;
-//                 to.balance += amount;
-//                 println!(
-//                     "Transferred ${:.2} from Account {} to Account {}.",
-//                     amount,from.account_number, to.account_number
-//                 );
-//             } else {
-//                 println!(
-//                     "Transfer failed: Account {} has insufficient funds (${:.2})",
-//                     from.account_number, from.balance
-//                 );
-//             }
-//         }
+//         Ok(Config { query , file_path })
 //     }
 // }
- 
+//
+// pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+//
+//     let content = fs::read_to_string(config.file_path)?;
+//
+//     println!("\nWith the following text: \n {content}");
+//
+//     Ok(())
+// }
+//
+// // example for not use Vec, faster and the same understanding
+// // pub fn search<'a>(query: &str, contents: &'a str) {/* -> Vec<&'a str> { */
+//
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     let mut result = Vec::new();
+//
+//     for line in contents.lines() {
+//         if line.contains(query) {
+//             result.push(line);
+//             println!("Eureka! It worked! :D\n");
+//             println!("fn search()\n");
+//         }
+//     }
+//
+//     result
+// }
+//
+// pub fn search_case_insensitive<'a>(
+//     query: &str,
+//     contents: &'a str
+// ) -> Vec<&'a str> {
+//
+//     let query = query.to_lowercase();
+//
+//     let mut result = Vec::new();
+//
+//     for line in contents.lines() {
+//         if line.to_lowercase().contains(&query) {
+//             result.push(line);
+//             println!("fn search_case_insensitive()\n");
+//         }
+//     }
+//
+//     result
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn case_sensitive() {
+//         let query = "duct";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three
+// Duct tape.";
+//         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+//     }
+//
+//     #[test]
+//     fn case_insensitive() {
+//         let query = "RuSt";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Trust me.";
+//
+//         assert_eq!(
+//             vec!["Rust:", "Trust me."],
+//             search_case_insensitive(query, contents)
+//         );
+//     }
+// }
+//
+//
+//
+//
+//
+// // pub mod banking {
+// //     pub mod accounts {
+// //         #[derive(Debug)]
+// //         pub struct Account {
+// //             pub account_number: u32,
+// //             pub balance: f64,
+// //         }
+// //
+// //         pub fn open_account(id: u32) -> Account {
+// //             println!("Account {} opened!", id);
+// //             Account { account_number: (id), balance: (0.0) }
+// //         }
+// //
+// //         #[allow(dead_code)]
+// //         fn close_account(account: &mut Account) {
+// //             println!("Account {} closed!", account.account_number);
+// //             account.balance = 0.0;
+// //         }
+// //     }
+// //
+// //     pub mod transactions {
+// //         // super allows us accese to functionality outside
+// //         // the "transactions" module
+// //         use super::accounts::Account;
+// //
+// //         pub fn deposit(account: &mut Account, amount: f64) {
+// //             account.balance += amount;
+// //             println!(
+// //                 "Deposited ${:.2} into Account {}. New balance: {:.2}",
+// //                 amount, account.account_number, account.balance
+// //             );
+// //         }
+// //
+// //         pub fn withdraw(acc: &mut Account, amount: f64) {
+// //             if amount > acc.balance {
+// //                 println!(
+// //                     "[TRANSACTION] ERROR: Insufficient funds for ${} withdraw from account 
+// //                     {} with balance ${:.2}",
+// //                     amount, acc.account_number, acc.balance
+// //                 );
+// //             } else {
+// //                 acc.balance -= amount;
+// //                 println!(
+// //                     "[TRANSACTION] Withdraw ${:.2} from Account {}. New balance: ${:.2}",
+// //                     amount, acc.account_number, acc.balance
+// //                 );
+// //             }
+// //         }
+// //
+// //         pub fn transfer(from: &mut Account, to: &mut Account, amount: f64) {
+// //             if from.balance >= amount {
+// //                 from.balance -= amount;
+// //                 to.balance += amount;
+// //                 println!(
+// //                     "Transferred ${:.2} from Account {} to Account {}.",
+// //                     amount,from.account_number, to.account_number
+// //                 );
+// //             } else {
+// //                 println!(
+// //                     "Transfer failed: Account {} has insufficient funds (${:.2})",
+// //                     from.account_number, from.balance
+// //                 );
+// //             }
+// //         }
+// //     }
+// // }
+//
 
--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,178 @@
-// let's try to do a mini grep example
-// extracting a word from a textfile
 
-use::std::env::args;
-use std::{env, error::Error, fs, process};
 
-use rust_playground::{search, search_case_insensitive};
-use::rust_playground::Config; // lib.rs
 
-pub struct Configuration {
-    pub query: String,
-    pub file_path: String,
-    pub ignore_case: bool,
-}
 
-impl Configuration {
-    fn build(args: &[String]) -> Result<Configuration, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Configuration { 
-            query, 
-            file_path, 
-            ignore_case 
-        })
-    }
-}
 
-fn run(config: Configuration) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search(&config.query, &contents)
-    };
-    
-    for line in results {
-        println!("{line}");
-    }
 
-    Ok(())
-}
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Configuration::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
 
-    if let Err(e) = run(config) {
-        println!("Application error: {e}");
-        process::exit(1);
-    }
-}
 
-// fn main() {
+
+
+
+
+
+
+// ---------------------------------------------------
+
+// // let's try to do a mini grep example
+// // extracting a word from a textfile
 //
-//     //Splitting Code into a Library Crate
-//     
-//     let args: Vec<String> = args().collect();
+// use::std::env::args;
+// use std::{env, error::Error, fs, process};
 //
-//     // using or lib.rs
-//     // let config = Config::build(&args).unwrap_or_else(|err| {
-//     //     println!("Problem parsing arguments: {err}");
-//     //     process::exit(1);
-//     // });
-//     //
-//     // if let Err(e) = rust_playground::run(config) {
-//     //     println!("Application error: {e}");
-//     //     process::exit(1);
-//     // }
+// use rust_playground::{search, search_case_insensitive};
+// use::rust_playground::Config; // lib.rs
 //
-//     let uwu = "\
-//         qwerty
-//         asdf
-//         pepito
-//         rust";
-//
-//     let owo = rust_playground::search("pepito", uwu);    
-//
-//     // let args: Vec<String> = args().collect();
-//     // let query = &args[1];
-//     // let file = &args[2];
-//
-//     // dbg!(&query, &file);
-//
-//     // let content = fs::read_to_string(&file)
-//         // .expect("::: There's no such file! :::'");
-//     // let config = parse_config(&args);
-//    
-//     // 1st from ---------------------------------
-//     // let config = match Config::new(&args) {
-//     //     Ok(c) => c,
-//     //     Err(e) => {
-//     //         eprintln!("Error: {}", e);
-//     //         std::process::exit(1);
-//     //     }
-//     // };
-//     
-//     // 2nd form ---------------------------------
-//     // let config = Config::build(&args).unwrap_or_else(
-//     //     |err| {
-//     //         println!("Error parsing args {err}");
-//     //         process::exit(1);
-//     //     }
-//     // );
-//
-//     // 3rd form ----------------------------------
-//     // run(config);
-//
-//     // let content = fs::read_to_string(config.file_path)
-//     //     .expect("::: There's no such file! :::'");
-//
-//     // dbg!(content);
-//
-//     // dbg!(parse_config(&args));
-//     
-//     // Handling Errors Returned from run in main
-//     // if let Err(e) = run(config) {
-//     //     println!("Application error: {e}");
-//     //     process::exit(1);
-//     // }
+// pub struct Configuration {
+//     pub query: String,
+//     pub file_path: String,
+//     pub ignore_case: bool,
 // }
-
-
-// impl Config {
-//     fn new(args: &[String]) -> Config {
-//         // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#improving-the-error-message
+//
+// impl Configuration {
+//     fn build(args: &[String]) -> Result<Configuration, &'static str> {
 //         if args.len() < 3 {
-//             panic!(":: Not enough arguments, mate!")
+//             return Err("Not enough arguments!");
 //         }
 //
 //         let query = args[1].clone();
 //         let file_path = args[2].clone();
 //
-//         Config { query , file_path }
+//         let ignore_case = env::var("IGNORE_CASE").is_ok();
+//
+//         Ok(Configuration { 
+//             query, 
+//             file_path, 
+//             ignore_case 
+//         })
 //     }
 // }
-
-
-// https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#grouping-configuration-values
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let file_path = args[2].clone();
 //
-//     Config { query, file_path }
+// fn run(config: Configuration) -> Result<(), Box<dyn Error>> {
+//     let contents = fs::read_to_string(config.file_path)?;
+//
+//     let results = if config.ignore_case {
+//         search_case_insensitive(&config.query, &contents)
+//     } else {
+//         search(&config.query, &contents)
+//     };
+//
+//     for line in results {
+//         println!("{line}");
+//     }
+//
+//     Ok(())
 // }
+//
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//
+//     let config = Configuration::build(&args).unwrap_or_else(|err| {
+//         println!("Problem parsing arguments: {err}");
+//         process::exit(1);
+//     });
+//
+//     if let Err(e) = run(config) {
+//         println!("Application error: {e}");
+//         process::exit(1);
+//     }
+// }
+//
+// // fn main() {
+// //
+// //     //Splitting Code into a Library Crate
+// //     
+// //     let args: Vec<String> = args().collect();
+// //
+// //     // using or lib.rs
+// //     // let config = Config::build(&args).unwrap_or_else(|err| {
+// //     //     println!("Problem parsing arguments: {err}");
+// //     //     process::exit(1);
+// //     // });
+// //     //
+// //     // if let Err(e) = rust_playground::run(config) {
+// //     //     println!("Application error: {e}");
+// //     //     process::exit(1);
+// //     // }
+// //
+// //     let uwu = "\
+// //         qwerty
+// //         asdf
+// //         pepito
+// //         rust";
+// //
+// //     let owo = rust_playground::search("pepito", uwu);    
+// //
+// //     // let args: Vec<String> = args().collect();
+// //     // let query = &args[1];
+// //     // let file = &args[2];
+// //
+// //     // dbg!(&query, &file);
+// //
+// //     // let content = fs::read_to_string(&file)
+// //         // .expect("::: There's no such file! :::'");
+// //     // let config = parse_config(&args);
+// //    
+// //     // 1st from ---------------------------------
+// //     // let config = match Config::new(&args) {
+// //     //     Ok(c) => c,
+// //     //     Err(e) => {
+// //     //         eprintln!("Error: {}", e);
+// //     //         std::process::exit(1);
+// //     //     }
+// //     // };
+// //     
+// //     // 2nd form ---------------------------------
+// //     // let config = Config::build(&args).unwrap_or_else(
+// //     //     |err| {
+// //     //         println!("Error parsing args {err}");
+// //     //         process::exit(1);
+// //     //     }
+// //     // );
+// //
+// //     // 3rd form ----------------------------------
+// //     // run(config);
+// //
+// //     // let content = fs::read_to_string(config.file_path)
+// //     //     .expect("::: There's no such file! :::'");
+// //
+// //     // dbg!(content);
+// //
+// //     // dbg!(parse_config(&args));
+// //     
+// //     // Handling Errors Returned from run in main
+// //     // if let Err(e) = run(config) {
+// //     //     println!("Application error: {e}");
+// //     //     process::exit(1);
+// //     // }
+// // }
+//
+//
+// // impl Config {
+// //     fn new(args: &[String]) -> Config {
+// //         // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#improving-the-error-message
+// //         if args.len() < 3 {
+// //             panic!(":: Not enough arguments, mate!")
+// //         }
+// //
+// //         let query = args[1].clone();
+// //         let file_path = args[2].clone();
+// //
+// //         Config { query , file_path }
+// //     }
+// // }
+//
+//
+// // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#grouping-configuration-values
+// // fn parse_config(args: &[String]) -> Config {
+// //     let query = args[1].clone();
+// //     let file_path = args[2].clone();
+// //
+// //     Config { query, file_path }
+// // }
 
