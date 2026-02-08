fb9326bd212f78b96d397493a8454c3eceed5e81
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-02-08 12:40:11 +0100

    feat: add listening for incoming streams and orinting a msg when we receive a stream

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::net::TcpListener;
 
+fn main() {
+    let listener = TcpListener::bind(
+        "127.0.0.1:6969"
+    ).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
+    
+}
 
 
 
