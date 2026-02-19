b89a42c35b908d932ffebe7efeb90814b19e64ec
0LE6 <135649528+0LE6@users.noreply.github.com>
2026-02-19 21:58:37 +0100

    feat: update  Explicitly breaking out of the loop when recv returns an error

--- src/lib.rs
+++ src/lib.rs
@@ -21,11 +21,18 @@ impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
+                let message = receiver.lock().unwrap().recv();
 
-                println!("Worker {id} got a job; executing.");
-
-                job();
+                match message {
+                    Ok(job) => {
+                        println!("Worker {id} got a job; executing.");
+                        job();
+                    }
+                    Err(_) => {
+                        println!("Worker {id} got a job; executing.");
+                        break;
+                    }
+                }
             }
         });
 
